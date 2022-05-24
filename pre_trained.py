1196961e0832aa5fc2b3917b2a5b80db6b594582
kmc0207 <kmc020700@gmail.com>
2022-05-24 21:04:31 +0900

    pre

--- pre_trained.py
+++ pre_trained.py
@@ -1,13 +1,20 @@
 from multi_run import RUN
 import torch
+from resnet import Reduced_ResNet18
+from model import ResNet18, reduced_ResNet18,SupConResNet
 import numpy as np
 
 def main():
     if torch.cuda.is_available():
         R = RUN('cuda:0',data='CIFAR10')
+        model = torch.load('model.pt').to('cuda:0')
     else:
         R = RUN('cpu',data="CIFAR10")
-    model = torch.load('model.pt')
+        model = torch.load('model.pt').to('cpu')
+    
     a = R.test(model,R.tls)
     print(a)
     print('mean : ', np.array(a).mean())
+    
+    
+main()
